b2ef663aa21be139631cb162508fa1c430e67457
Nikita Utkin <utkin@mindbox.cloud>
2024-01-02 22:31:08 +0500

    Dummy dumb

--- accountant/command/_util.py
+++ accountant/command/_util.py
@@ -10,7 +10,7 @@ GROUP_LIKE = [ChatType.GROUP, ChatType.SUPERGROUP, ChatType.CHANNEL]
 def chat_type(chat_type: ChatType | Iterable[ChatType]):
     def decorator(handler):
         async def wrapped(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-            if isinstance(ChatType) and update.effective_chat.type != chat_type:
+            if isinstance(chat_type, ChatType) and update.effective_chat.type != chat_type:
                 return
             if update.effective_chat.type not in chat_type:
                 return
